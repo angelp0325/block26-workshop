880b74df40712d43fb36c1f6511612fa70237ed9
Angel Polanco <angelepolanco325@gmail.com>
2025-11-11 21:46:51 -0500

    added app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import React from "react";
+import { GameProvider } from "./context/GameContext";
+import useGame from "./hooks/useGame";
+import WelcomeScreen from "./components/WelcomeScreen";
+import GameBoard from "./components/GameBoard";
+import "./index.css";
+
+function GameView() {
+  const { gameState } = useGame();
+  return gameState === "welcome" ? <WelcomeScreen /> : <GameBoard />;
+}
+
 export default function App() {
-  return <></>;
+  return (
+    <GameProvider>
+      <GameView />
+    </GameProvider>
+  );
 }
